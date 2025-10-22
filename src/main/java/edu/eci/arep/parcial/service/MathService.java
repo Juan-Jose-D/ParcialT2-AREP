8692a2a653a0e6a8197fcf1297811bf59c921ffb
unknown <juan.dgomez@labinfo.is.escuelaing.edu.co>
2025-10-22 09:25:58 -0500

    Servicio math inicial

--- src/main/java/edu/eci/arep/parcial/service/MathService.java
+++ src/main/java/edu/eci/arep/parcial/service/MathService.java
@@ -1,5 +1,28 @@
 package edu.eci.arep.parcial.service;
 
+import java.util.ArrayList;
+
 public class MathService {
-    
+
+    private ArrayList<Integer> resultado;
+    private ArrayList<Integer> resultado2;
+
+    public ArrayList<Integer> numerosCatalan(Integer numero){
+        Integer sumatoria = 1;
+
+        for (Integer i = 1; i <= numero - 1; i++){
+            Integer actual = i * (sumatoria - 1 - i);
+            sumatoria += actual;
+            resultado.add(sumatoria);
+            System.out.println(resultado);
+        }
+
+        for (Integer i = 1; i <= numero - 1; i++){
+            Integer actual2 = 1/(i+1)*((2*i)/i);
+            resultado2.add(actual2);
+            System.out.println(resultado2);
+
+        }
+        return resultado;
+    }
 }
